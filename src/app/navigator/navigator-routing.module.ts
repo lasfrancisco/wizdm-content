668f4147dacc5206b8f1dff607eb0a598bcd5ce6
lasfrancisco <admin@bizdemon.com>
2020-06-10 15:43:55 +0200

    1 file modified

--- src/app/navigator/navigator-routing.module.ts
+++ src/app/navigator/navigator-routing.module.ts
@@ -1,57 +1,16 @@
 import { NgModule, Injectable, Inject } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
 import { ContentSelector, ContentConfigurator, ContentRouterModule, RoutesWithContent, AllowedContent } from '../content';
 import { NavigatorComponent } from './navigator.component';
-import { of } from 'rxjs';
-import { first, delay, map } from 'rxjs/operators';
 
-@Injectable() 
-class MyAsyncSeletor extends ContentSelector {
+@Injectable({ providedIn: 'root' })
+class test implements CanActivate {
 
-  private current = this.config.defaultValue;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-  constructor(router: Router, config: ContentConfigurator){ super(router, config); }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): AllowedContent {
-
-    console.log("Simulating asyncronous user authentication check");
-
-    return of(null).pipe( 
-
-      first(),
-      
-      delay(100),
-
-      map(user => {
-        // Gets the language code requested from the route 
-        const requested = route.paramMap.get( this.config.selector );
-        console.log('Requested language:', requested);
-
-        const preferred = user && user.lang;
-        console.log('User preferred language:', preferred);
-
-        // Selects the best language among the allowed ones 
-        const selected = this.languageAllowed( 
-          
-          requested === 'auto' ? 
-        
-          // Detects the langage automatically on request  
-          ( preferred || this.browserLanguage ) : 
-          
-          // Replaces wildcards with the current language
-          ( requested === '**' ? this.current : requested )
-        
-        );
-
-        console.log('Selected language:', selected);
-
-        // Keeps track of  the currently selected language
-        this.current = selected;
-
-        // Redirects to the selected whenever differs from the requested
-        return(requested !== selected ? this.switchLanguage(selected, state) : true) as AllowedContent;
-      }) 
-    );
+    console.log(route, state);
+    
+    return true;
   }
 }
 
@@ -59,7 +18,7 @@ const routes: RoutesWithContent = [
   { path: '', redirectTo: 'auto', pathMatch: 'full' },
   { 
     path: ':lang', component: NavigatorComponent, 
-    canActivate: [ MyAsyncSeletor ],
+    canActivate: [ test, ContentSelector ],
     content: 'navigator',
     
     children: [
@@ -79,6 +38,6 @@ const routes: RoutesWithContent = [
 @NgModule({
   imports: [ ContentRouterModule.forChild(routes) ],
   exports: [ ContentRouterModule ],
-  providers: [ MyAsyncSeletor ]
+  providers: [ ContentSelector ]
 })
 export class NavigatorRoutingModule { }
